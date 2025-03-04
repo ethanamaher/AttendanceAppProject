54e2beccfea44f4633432af29930b3d0c2891b7b
ethan <ethanamaher@gmail.com>
2025-03-04 14:28:21 -0600

    compile student response information into a student object, will need to add all student objects to a dictionary when we want to send to database

---
+++ AttendanceAppProject.Web/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace AttendanceAppProject.Web
+{
+  public class Student
+  {
+    public string id;
+    public string name;
+
+    public Student(string id, string name)
+    {
+      this.id = id;
+      this.name = name;
+    }
+
+    public override string ToString()
+    {
+      return id + ", " + name;
+    }
+  }
+}
