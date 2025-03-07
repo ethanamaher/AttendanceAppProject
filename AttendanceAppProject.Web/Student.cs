e87c778cabb09adb27818a43328e3ecd9039a8aa
ethan <ethanamaher@gmail.com>
2025-03-07 15:55:21 -0600

    change student exists check to be from student response, added password field, dont create student if password is incorrect

--- AttendanceAppProject.Web/Student.cs
+++ AttendanceAppProject.Web/Student.cs
@@ -5,17 +5,12 @@
     public string id;
     public string name;
 
+
     public Student(string id, string name)
     {
       this.id = id;
       this.name = name;
     }
-
-    public Boolean Exists()
-    {
-      return id?.Length > 0 && name?.Length > 0;
-    }
-
     public override string ToString()
     {
       return id + ", " + name;
