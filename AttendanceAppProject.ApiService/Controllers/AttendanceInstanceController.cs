b03aab1285f09ea865e939244ca47175a9437865
Maaz Raza <maazpm@gmail.com>
2025-03-25 18:51:27 -0500

    Added attendance instance controller, added post request to class controller

---
+++ AttendanceAppProject.ApiService/Controllers/AttendanceInstanceController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AttendanceAppProject.ApiService.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using AttendanceAppProject.ApiService.Data;
+using AttendanceAppProject.Dto.Models;
+
+namespace AttendanceAppProject.ApiService.Controllers
+{
+    [Route("api/[controller]")] // Automatically becomes "api/AttendanceInstance"
+    [ApiController]
+    public class AttendanceInstanceController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendanceInstanceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AttendanceInstance
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AttendanceInstance>>> GetAttendanceInstances()
+        {
+            return await _context.AttendanceInstances.ToListAsync();
+        }
+
+        // POST: api/AttendanceInstance
+        [HttpPost]
+        public async Task<ActionResult<AttendanceInstance>> AddAttendanceInstance([FromBody] AttendanceInstanceDto dto)
+        {
+            var attendance = new AttendanceInstance
+            {
+                AttendanceId = dto.AttendanceId,
+                StudentId = dto.StudentId,
+                ClassId = dto.ClassId,
+                IpAddress = dto.IpAddress,
+                IsLate = dto.IsLate,
+                ExcusedAbsence = dto.ExcusedAbsence,
+                DateTime = dto.DateTime
+            };
+
+            _context.AttendanceInstances.Add(attendance);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAttendanceInstances), new { id = attendance.AttendanceId }, attendance);
+        }
+    }
+}
--- AttendanceAppProject.ApiService/Controllers/ClassController.cs
+++ AttendanceAppProject.ApiService/Controllers/ClassController.cs
@@ -2,6 +2,7 @@
 using AttendanceAppProject.ApiService.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using AttendanceAppProject.ApiService.Data;
+using AttendanceAppProject.Dto.Models;
 
 namespace AttendanceAppProject.ApiService.Controllers
 {
@@ -16,11 +17,34 @@ namespace AttendanceAppProject.ApiService.Controllers
 			_context = context;
 		}
 
-		// GET: api/Student
+		// GET: api/Class
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<Class>>> GetClasses()
 		{
 			return await _context.Classes.ToListAsync();
 		}
-	}
+
+        // POST: api/Class
+        [HttpPost]
+        public async Task<ActionResult<Class>> AddClass([FromBody] ClassDto dto)
+        {
+            var newClass = new Class
+            {
+                ClassId = Guid.NewGuid(), // Auto-generate
+                ProfUtdId = dto.ProfUtdId,
+                ClassPrefix = dto.ClassPrefix,
+                ClassNumber = dto.ClassNumber,
+                ClassName = dto.ClassName,
+                StartDate = dto.StartDate,
+                EndDate = dto.EndDate,
+                StartTime = dto.StartTime,
+                EndTime = dto.EndTime
+            };
+
+            _context.Classes.Add(newClass);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetClasses), new { id = newClass.ClassId }, newClass);
+        }
+    }
 }
