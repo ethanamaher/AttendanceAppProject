6a3addb36a8b4fdec7e605549a44da091151c3ba
Maaz Raza <maazpm@gmail.com>
2025-03-28 17:06:11 -0500

    added StudentClass DTO

---
+++ AttendanceAppProject.Dto/Models/StudentClassDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceAppProject.Dto.Models
+{
+    public class StudentClassDto
+    {
+        public Guid StudentClassId { get; set; } 
+
+        public string StudentId { get; set; } = null!;  
+
+        public Guid ClassId { get; set; }           
+
+    }
+}
