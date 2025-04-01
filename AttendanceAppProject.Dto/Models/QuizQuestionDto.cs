6d4ef43b7d19a565bd9596d5f5896e8fa66d4ba9
Maaz Raza <maazpm@gmail.com>
2025-04-01 10:56:31 -0500

    updated comments

--- AttendanceAppProject.Dto/Models/QuizQuestionDto.cs
+++ AttendanceAppProject.Dto/Models/QuizQuestionDto.cs
@@ -5,7 +5,7 @@ namespace AttendanceAppProject.Dto.Models;
 
 public class QuizQuestionDto
 {
-    // Nullable so that the client doesn't need to provide an ID when creating a new attendance instance.
+    // Nullable so that the client doesn't need to provide an ID when creating a new QuizQuestion.
     // The server (API controller) will auto-generate a new GUID and assign it when saving to the database.
     // When retrieving data (from GET requests), this field will be populated with the actual value from the DB.
     public Guid? QuestionId { get; set; } // PK
--- AttendanceAppProject.Dto/Models/StudentClassDto.cs
+++ AttendanceAppProject.Dto/Models/StudentClassDto.cs
@@ -8,7 +8,7 @@ namespace AttendanceAppProject.Dto.Models
 {
     public class StudentClassDto
     {
-        // Nullable so that the client doesn't need to provide an ID when creating a new attendance instance.
+        // Nullable so that the client doesn't need to provide an ID when creating a new StudentClass mapping.
         // The server (API controller) will auto-generate a new GUID and assign it when saving to the database.
         // When retrieving data (from GET requests), this field will be populated with the actual value from the DB.
         public Guid? StudentClassId { get; set; }  // PK
