65aba2f5cb82ef13cd07e8685b3416003a00dfc0
ethan <ethanamaher@gmail.com>
2025-04-05 13:23:59 -0500

    work on implementing quiz, changed database schema to accomodate quizquestions

---
+++ AttendanceAppProject.ApiService/Controllers/QuizAnswerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AttendanceAppProject.ApiService.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using AttendanceAppProject.ApiService.Data;
+using AttendanceAppProject.Dto.Models;
+
+// API Controller for Password
+
+namespace AttendanceAppProject.ApiService.Controllers
+{
+    [Route("api/[controller]")] // Automatically becomes "api/password"
+    [ApiController]
+    public class QuizAnswerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuizAnswerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /* GET: api/quizquestions
+         * Get all passwords
+         * - request body: none
+         * - response body: Passwords
+         */
+        [HttpGet("{QuestionId}")]
+        public async Task<ActionResult<IEnumerable<QuizQuestion>>> GetAnswersById(Guid QuestionId)
+        {
+            System.Diagnostics.Debug.WriteLine($"Looking up answers for {QuestionId}");
+            return await _context.QuizQuestions.Where(qanswer => qanswer.QuestionId == QuestionId).ToListAsync();
+        }
+    }
+}
+
---
+++ AttendanceAppProject.Dto/Models/QuizAnswerDto.cs
@@ -0,0 +1,21 @@
+﻿/* Quiz Question Dto, used to transfer QuizQuestion data between the server and client side and encapsulate JSON responses from HTTP requests for client to interact with 
+ * Written by Maaz Raza
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace AttendanceAppProject.Dto.Models;
+
+public class QuizAnswerDto
+{
+    // Nullable so that the client doesn't need to provide an ID when creating a new QuizQuestion.
+    // The server (API controller) will auto-generate a new GUID and assign it when saving to the database.
+    // When retrieving data (from GET requests), this field will be populated with the actual value from the DB.
+    public int? AnswerId { get; set; }
+    public Guid? QuizId { get; set; }
+    public Guid? QuestionId { get; set; }
+    public bool? IsCorrect { get; set; }
+
+
+}
