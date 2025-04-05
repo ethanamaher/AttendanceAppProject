863abe001d74fb141a4a65c6819906ed9d6353e8
Maaz Raza <maazpm@gmail.com>
2025-04-05 11:43:49 -0500

    added ability to make a student late or excused absence to AttendanceInstanceController

--- AttendanceAppProject.ApiService/Controllers/AttendanceInstanceController.cs
+++ AttendanceAppProject.ApiService/Controllers/AttendanceInstanceController.cs
@@ -59,5 +59,30 @@ namespace AttendanceAppProject.ApiService.Controllers
             // this is the conventional way, to return HTTP 201 created code and a Location header pointing to where the new resource can be found, and the new resource itself in the response body 
             return CreatedAtAction(nameof(GetAttendanceInstances), new { id = attendance.AttendanceId }, attendance);
         }
+
+        /* POST: api/AttendanceInstance/absent-or-late
+         * Add an attendnace instance to the database for students who were absent or late, entered on the client side by the professor
+         * - request body: AttendanceInstanceDto (must have either IsLate or IsAbsent set to true)
+         * - response body: AttendanceInstance 
+         */
+        [HttpPost("absent-or-late")]
+        public async Task<ActionResult<AttendanceInstance>> AddAbsentOrLate([FromBody] AttendanceInstanceDto dto)
+        {
+            var newAttendance = new AttendanceInstance
+            {
+                AttendanceId = Guid.NewGuid(),
+                StudentId = dto.StudentId,
+                ClassId = dto.ClassId,
+                IsLate = dto.IsLate ?? false, // either we set the IsLate to the value specified by the DTO, or we set it to false (in the case of this student being an absence and not late)
+                ExcusedAbsence = dto.ExcusedAbsence ?? false, // either we set the ExcusedAbsence to the value specified by the DTO, or we set it to false (in the case of this student being late and not absent)
+                DateTime = dto.DateTime ?? DateTime.Now,
+                IpAddress = "127.0.0.1" // since it is manually entered by professor we will use a placeholder IP
+            };
+
+            _context.AttendanceInstances.Add(newAttendance);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAttendanceInstances), new { id = newAttendance.AttendanceId }, newAttendance);
+        }
     }
 }
